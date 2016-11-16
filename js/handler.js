48ad898fd55fa42c772ab81466c45e828757891e
aqd14 <aqd14@msstate.edu>
2016-11-15 21:35:57 -0600

    + Fix bug wrong color when double click
    + Remove unused code

--- js/handler.js
+++ js/handler.js
@@ -3,7 +3,6 @@
 */
 
 var disease_list = ['Total', 'Pneumonia','Diarrhoea','Malaria','Aids','Measles','Injury','Meningitis','Other'];
-// d3.select(window).on("resize", throttle);
 
 var topo,projection,path,svg;
 
@@ -24,7 +23,6 @@ var max_value;
 var min_value;
 
 var zoom = d3.zoom()
-    //.extent([1,9])
     .scaleExtent([1, 9])
     .on("zoom", move);
 
@@ -35,33 +33,22 @@ var height = 850;
 setup(width,height);
 
 function setup(width,height){
-  //projection = d3.geo.mercator()
   projection = d3.geoMercator()
     .translate([(width/2), (height/2)])
     .scale( width / 2 / Math.PI);
 
-  //path = d3.geo.path().projection(projection);
   path = d3.geoPath().projection(projection);
 
   svg = d3.select("#chart").append("svg")
-      //.attr("id", 'world')
       .attr("width", width)
       .attr("height", height)
       .call(zoom)
-      //.on("click", click)
       .append("g");
 
   g = svg.append("g")
          .on("click", click);
 
 }
-
-// var scale = colorbrewer.YlOrRd[9].reverse().concat(colorbrewer.YlGn[9]);
-// The color scale
-
-// The SVG container
-/*var width  = 960;
-var height = 550;*/
 
 // The network of lines of latitude and longitude upon which a map is drawn
 var graticule = d3.geoGraticule();
@@ -100,17 +87,12 @@ d3.json("topojson/world-topo-min.json", function(error, world) {
 });
 
 function drawMap() {
-/*	svg.append("path")
-	   	.datum(graticule)
-		  .attr("class", "graticule")
-		  .attr("d", path);*/
   initMapParams()
 
 	g.append("path")
 	 .datum({type: "LineString", coordinates: [[-180, 0], [-90, 0], [0, 0], [90, 0], [180, 0]]})
 	 .attr("class", "equator")
 	 .attr("d", path);
-
 
   var country = g.selectAll(".country").data(topo);
   country.enter().insert("path")
@@ -148,8 +130,6 @@ function handleMouseClick(){
 	Display mortality rate when user hovers mouse over each country
 */
 function handleMouseOver(country) {
-        // ** Show/hide tooltip
-   
     var mouse = d3.mouse(svg.node()).map( function(d) { return parseInt(d); } );
     // Check mortality rate for each country
     // Assign 'undefine' if not available
@@ -162,6 +142,7 @@ function handleMouseOver(country) {
 			.html('<b>'+country.properties.name+'</b>'+' <span class="sep"> - </span> '+ mortality_rate);
 }
 
+// Hide tooltip
 function handleMouseOut() {
 	tooltip.classed("hidden", true)
 }
@@ -194,10 +175,6 @@ function move() {
 
   //zoom.translateBy(t);
   g.attr("transform", "translate(" + t + ")scale(" + s + ")");
-
-  //adjust the country hover stroke width based on zoom level
-//    d3.selectAll(".country").style("stroke-width", 1.5 / s);
-
 }
 
 var throttleTimer;
@@ -226,20 +203,14 @@ $("#Total").click(function(d, i) {
 
 // Handle event click on separate disease
 $( "#separate-disease .selection" ).click(function(d, i) {
-  console.log( 'Click on disease' + $(this).index());
   if (disease_index !== ($(this).index() + 1)) {
     disease_index = $(this).index() + 1;
-  //  var diseases = disease_list[disease_index];
     update();
   }
 });
 
 /* Update map color based on the disease's rate */
 function update() {
-
-  // Get data from selected year
-  curYear = +d3.select("#year").node().value;
-//  data = getDataCurrentYear(curYear);
   switch(disease_index) {
     case 0: // Total rate
       disease_color = d3.schemeReds[sub_range];
@@ -273,13 +244,14 @@ function update() {
       break;
   }
 
+  // Update max/min value
+  updateValues();
+  // Get main color
   color = getDiseaseColor();
   // Update choropleth map
   updateMap();
   // Update legends
   updateLegendColor(disease_list[disease_index]);
-  // Update max/min value
-  updateValues();
 //  d3.select("#yearLabel").text(2000);
 }
 
@@ -288,8 +260,6 @@ function updateValues() {
   values = d3.values(data).map(function(d) { return d[disease_index]; })
   max_value = +d3.max(values);
   min_value = +d3.min(values);
-  // console.log('Max value: ' + max_value);
-  // console.log('Min value: ' + min_value);
 }
 
 // Return data corressponding with selected year
@@ -305,11 +275,6 @@ function getDiseaseColor() {
     value = parseFloat(min_value + diff*sub_range_index).toFixed(1);
     color_domain.push(value);
   }
-  // [min_value, min_value + diff/4, min_value + diff/3, min_value + diff/2, max_value];
-  // Round elements
-/*  for (var i = 0; i < color_domain.length; i++) {
-    color_domain[i] = parseFloat(color_domain[i]).toFixed(1);
-  } */
   var color = d3.scaleQuantile()
     .domain(color_domain)
     .range(disease_color);
@@ -318,9 +283,6 @@ function getDiseaseColor() {
 
 /* Update the color and data in wolrd map */
 function updateMap() {
-/*  console.log("Call updateMap function!");
-  console.log("Disease index: " + disease_index);
-  console.log("Color: " + color.range());*/
   d3.selectAll('.country').style("fill", function(d, i) {
       if (data[d.properties.name] !== undefined) {
         return color(data[d.properties.name][disease_index]);
@@ -331,9 +293,7 @@ function updateMap() {
 
 /* Create chorolepath threshold */
 function updateLegendColor(disease_name) {
-  console.log("In function createLegendColor!");
   d3.selectAll(".swatch").style("background", function(d, i) {
-    console.log("Color: " + disease_color[i]);
     return disease_color[i];
   });
 }
